827f369899860c73019929c7c2fe6adebacf962c
dhruvpatel18182@gmail.com <dhruvpatel18182@gmail.com>
2025-08-20 21:35:13 +0530

    First Commite

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -26,7 +26,7 @@ namespace Billing.Controllers
                {
                    return Ok(new
                    {
-                       Message = "Hello from a private-scoped endpoint!"
+                       Message = "Hello from a private-scoped endpoint! Hello Dhruv"
                    });
                }
     }
