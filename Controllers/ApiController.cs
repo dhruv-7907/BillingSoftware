80f5ab7bf38e58f88a3231cb08e693b2d13d645b
dhruvpatel18182@gmail.com <dhruvpatel18182@gmail.com>
2025-09-08 23:02:26 +0530

    dhruv commite

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -26,7 +26,7 @@ namespace Billing.Controllers
                {
                    return Ok(new
                    {
-                       Message = " I am Ishan Patel hello Dhruv"
+                       Message = "Dhruv Patel"
                    });
                }
     }
